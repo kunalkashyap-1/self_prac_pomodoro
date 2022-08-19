3bcec91eb7d2bee9188dd28f85b87b06bedf84ae
kunalkashyap-1 <kunal.kashyap.8775@gmail.com>
2022-08-19 20:46:19 +0530

    Added timer and background sound init

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-const sortBreak=25;
-const normalBreak=25;
-const longBreak=25;
+const sortBreak = 25;
+const normalBreak = 25;
+const longBreak = 25;
+
+$(".start_btn").click(startTimer);
+$(".start_btn").click(playSound);
 
 
+function startTimer() {
+    let min = sortBreak;
+    let sec = min * 60;
+    setInterval(mytimer, 1000);
+    function mytimer() {
+        let time = min + " : " + (sec % 60);
+        if (sec % 60 === 0) {
+            min--;
+        }
+        sec--;
+        $("#time").text(time);
+    }
 
-function toggle_dropdown(){
+}
+
+function playSound(){
+    const audio=new Audio("sounds/lofi.mp3");
+    audio.play();
+}
+
+
+function toggle_dropdown() {
     $(".drop-list").toggle(".visible")
 }
